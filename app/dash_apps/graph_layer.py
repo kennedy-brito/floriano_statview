ebbee1ba6fde9de915e59503e43165776f1b06c0
kennedy-brito <willian.kennedy.brito@gmail.com>
2025-04-23 14:36:37 -0300

    styling projects

--- app/dash_apps/graph_layer.py
+++ app/dash_apps/graph_layer.py
@@ -3,6 +3,7 @@ import numpy as np
 import plotly.express as px
 from plotly.graph_objs import Figure
 from . import data_layer as data
+import plotly.graph_objects as go
 
 def format_pib(value):
     if value >= 1_000_000_000:
@@ -17,27 +18,52 @@ def age_pyramid()->Figure:
     data_frame=data.get_age_group(),
     x='valor',
     y='grupo_idade',
-    orientation='h')
+    orientation='h',
+    labels={'grupo_idade':'Grupo', 'valor': 'População'},
+    )
+
+  graph.update_layout(dict(
+    font_family='Segoe UI',
+    font_weight=700
+  ))
 
   return graph
 
 def most_populated_cities()->Figure:
-  graph = px.bar(
-    data_frame=data.get_top_population_city(),
-    x='populacao',
-    y='municipio',
+  df = data.get_top_population_city()
+  
+  floriano_idx = df[df['municipio'] == "Floriano"].index[0]
+  colors = ['#636EFA'] * len(df) #standard blue of plotly
+  colors[floriano_idx] = '#EF553B' #standard red of plotly
+  
+  graph = go.Bar(
+    x=df['populacao'],
+    y=df['municipio'],
     orientation='h',
+    marker_color=colors
     )
+  
 
-  graph.update_layout(yaxis=dict(autorange="reversed"))
-
-  return graph
+  fig = go.Figure(data=[
+    graph    
+  ])
+  
+  fig.update_layout(
+    yaxis=dict(autorange="reversed"),
+    xaxis_title="População",
+  
+    font_family='Segoe UI',
+    font_weight=600,
+    yaxis_title="Município")
+  
+  return fig
 
 def race_distribution()->Figure:
   graph = px.pie(
     data_frame=data.get_population_by_race(),
     names='raca',
-    values='porcentagem'
+    values='porcentagem',
+    labels={'raca':"Raça", 'porcentagem': "Porcentagem"}
   )
 
   return graph
@@ -46,7 +72,8 @@ def location_distribution()->Figure:
   graph = px.pie(
     data_frame=data.get_population_by_local(),
     names='local',
-    values='porcentagem'
+    values='porcentagem',
+    labels={'local':"Zona", 'porcentagem': "Porcentagem"}
   )
 
   return graph
